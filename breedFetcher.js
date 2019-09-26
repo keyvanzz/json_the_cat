ff3e4c2757a4cfc2f6418bac4e4f42eb1e952ddf
Keyvan <k.kholghi@gmail.com>
2019-09-26 23:23:23 +0000

    Implemented Dynamic Breed Name

---
+++ breedFetcher.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const args = process.argv.slice(2);
+const breedName = args[0]
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    // console.log(typeof body);
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (!breed) {
+        console.log("Not Found!")
+        return;
+    }
+
+    console.log(breed.description);
+});
