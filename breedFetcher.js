dcd77793e417011c35d775a2f8a17bdbbbd26bd4
Keyvan <k.kholghi@gmail.com>
2019-09-27 00:53:20 +0000

    Refactored the code in breedFetcher.js by moving the main request logic into a function named fetchBreedDescription.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,23 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breedName = args[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
+const fetchBreedDescription = function (breedName, callback) {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+        // console.log('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+        // console.log(typeof body);
+        const data = JSON.parse(body);
+        const breed = data[0];
+        if (!breed) {
+            callback("Cat breed Not Found!", null);
+        } else {
+            callback(null, breed.description);
+        }
+    });
+};
 
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  // console.log(typeof body);
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (!breed) {
-    console.log("Not Found!");
-    return;
-  }
-
-  console.log(breed.description);
-});
+module.exports = { fetchBreedDescription };
